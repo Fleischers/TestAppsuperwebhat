25513ecf50312038e256ee5c60e509350b1fb739
Glib Fleischer Ischenko <Fleischer458@gmail.com>
2015-05-30 19:26:07 +0300

    tempo

--- index.js
+++ index.js
@@ -1,22 +1,91 @@
 var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var http = require('http');
-var events = require("events");
-var messagepool = "";
-var server = http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  // res.writeHead(200, { 'Content-Type': 'application/json' });
-  if (req.url != "/favicon.ico")
-  messagepool += req.url.slice(1,req.url.length) + "\n";
-  res.end(messagepool);
-  // res.end(JSON.stringify(req.url));
+var util = require('util');
+var querystring = require('querystring');
 
-//}).listen(3000, '192.168.1.200');
-}).listen(3000, '172.16.0.200');
-//}).listen(3000, 'localhost');
-server.on('connection', function (stream) {
-  console.log('someone connected!');
+// var events = require("events");
+var server = http.createServer();
+var messageArray = [];
+server.on('request', function (req,res) {
+
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+
+    switch (req.url) {
+        // case '/':
+        default:
+            console.log(new Date());
+            console.log('Connected! ' + req.method + "\n"
+            // + util.inspect(req.headers)
+            );
+            var message = new Message( "user1", req.url.slice(1,req.url.length), new Date());
+            messageArray.push(message);
+            res.end(JSON.stringify(messageArray));
+            break;
+            /*
+            var message = new Message( "user1", req.url.slice(1,req.url.length), new Date());
+            messageArray.push(message);
+
+
+            */
+        // case '/chat':
+        //     if (req.method == 'POST') {
+        //         console.log("[200] " + req.method + " to " + req.url);
+        //         var fullBody = '';
+        //
+        //         req.on('data', function(chunk) {
+        //             // append the current chunk of data to the fullBody variable
+        //             fullBody += chunk.toString();
+        //         });
+        //
+        //         req.on('end', function() {
+        //
+        //             // request ended -> do something with the data
+        //             res.writeHead(200, "OK", {'Content-Type': 'application/json'});
+        //
+        //             // parse the received body data
+        //             var decodedBody = querystring.parse(fullBody);
+        //
+        //             // output the decoded data to the HTTP response
+        //             res.write(util.inspect(decodedBody));
+        //             res.end();
+        //         });
+        //
+        //     } else {
+        //         console.log("[405] " + req.method + " to " + req.url);
+        //         res.writeHead(405, "Method not supported", {'Content-Type': 'text/html'});
+        //         res.end('<html><head><title>405 - Method not supported</title></head><body><h1>Method not supported.</h1></body></html>');
+        //     }
+        //     break;
+        // default:
+        //     res.writeHead(404, "Not found", {'Content-Type': 'text/html'});
+        //     res.end('<html><head><title>404 - Not found</title></head><body><h1>Not found.</h1></body></html>');
+        //     console.log("[404] " + req.method + " to " + req.url);
+    }
+
+
+
+
+    req.on('data', function(data) {
+        // buffer.write(data);
+        // console.log("Buffer: " + buffer);
+        console.log("Buffer: " + util.inspect(data));
+    });
 });
+server.on('data', function (data) {
+    console.log('data received: ' + data);
+});
+// server.listen(3000, '172.16.0.200');
+server.listen(3000, '172.16.63.255');
+
+function Message (username, message, timestamp) {
+    this.username = username;
+    this.message = message;
+    this.timestamp = timestamp;
+}
+// 172.16.63.255
+//}).listen(3000, 'localhost');
+
 var url = 'mongodb://localhost:27017/test';
 MongoClient.connect(url, function(err, db) {
   assert.equal(null, err);
